a017e262da3e9651183391acb8de8052d4761699
joao.arteiro <joao.arteiro@accenture.com>
2024-11-22 10:31:19 +0000

    Implement Edit and Delete invoices functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,8 +14,9 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-const createInvoice = async (formData: FormData) => {
+export const createInvoice = async (formData: FormData) => {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
@@ -42,4 +43,26 @@ const createInvoice = async (formData: FormData) => {
   redirect("/dashboard/invoices");
 };
 
-export default createInvoice;
+export const updateInvoice = async (id: string, formData: FormData) => {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+};
+
+export const deleteInvoice = async (id: string) => {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+};
