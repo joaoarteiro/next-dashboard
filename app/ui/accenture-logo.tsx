29172e084ede9dfbe4bb39cdd777c4d69ab1579b
Margarida Campos <58434060+margaridarmc@users.noreply.github.com>
2024-12-09 10:38:46 +0000

    Fixed accenture logo

--- app/ui/accenture-logo.tsx
+++ app/ui/accenture-logo.tsx
@@ -1,17 +1,14 @@
-import { GlobeAltIcon } from "@heroicons/react/24/outline";
-import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
 export default function AccentureLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <Image
-        src="/acclogo.png.png"
-        alt="Accenture logo"
-        width={200}
-        height={200}
-      />
-    </div>
+    <Image
+      src="/acclogo.png"
+      alt="Accenture logo"
+      width={200}
+      height={200}
+      style={{ objectFit: "contain" }}
+      layout="intrinsic"
+      priority
+    />
   );
 }
